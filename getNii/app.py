1c33b1cec024051811baaf61f45a4d9c878fd08a
Yuelong Li <yuelong.edu@gmail.com>
2023-10-22 18:14:37 -0700

    unzip service

---
+++ getNii/app.py
@@ -0,0 +1,59 @@
+import json
+import requests
+import boto3
+import os
+import os
+
+os.environ['CURL_CA_BUNDLE'] = ''
+import zipfile
+
+
+def lambda_handler(event, context):
+    # connect to the s3
+    s3 = boto3.client("s3")
+    # Get the bucket name and file key
+    location = json.loads(event["body"])
+    # {
+#                          "id": 0,
+#                          "link": "unknown",
+#                          "createdAt": "2023-06-03T06:57:33.000000Z",
+#                          "updatedAt": "2023-06-03T06:57:33.000000Z",
+#                          "status": "unavailable",
+#                          "database": "s3",
+#                          "fileName": "Miss Camylle Grady_results",
+#                          "location": `${JSON.stringify({bucket: bucket, key:keyInBucket})}`
+#                      }
+
+    file_key = location['Key']
+    bucket_name = location['Bucket']
+    # save zip  file to local
+    fj = "/tmp/a.zip"
+    s3 = boto3.resource("s3")
+    s3.Bucket(bucket_name).download_file(file_key, fj)
+    archive = zipfile.ZipFile(fj, 'r')
+    info = archive.read('info.json')
+    info = json.loads(info)
+#     {
+#                 "filename": "data/NC.nii.gz",
+#                 "id": 0,
+#                 "dim": 2,
+#                 "name": "Noise Covariance",
+#                 "type": "output"
+#             },
+#             {
+#                 "filename": "data/snr.nii.gz",
+#                 "id": 1,
+#                 "dim": 3,
+#                 "name": "SNR",
+#                 "type": "output"
+#             }
+    ledge = info['data']
+    for data in ledge:
+        file = archive.read(data['filename'])
+        target_key = f"{file_key}_{os.path.basename(file)}"
+        s3_client.put_object(Body=file, Bucket=bucket_name, Key=target_key)
+        # Generate a pre-signed URL for the uploaded .nii file
+        url = s3_client.generate_presigned_url('get_object',Params={'Bucket': target_bucket, 'Key': target_key},
+            ExpiresIn=3600)  # URL valid for 1 hour
+        data['link'] = url
+    return ledge
