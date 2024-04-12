36736de280c10c2ed47d15985499ff3efbfcbc6f
eros.montin <eros.montin@gmail.com>
2024-04-12 18:06:05 -0400

    fullapp tested on a different aws account

--- setup_only_FE.sh
+++ setup_only_FE.sh
@@ -1,17 +1,20 @@
 
 
-
 #Platform:
 CORTEX=cancelit-env-1.eba-pmamcuv5.us-east-1.elasticbeanstalk.com
 CLOUDMRSTACK=cmr
-_NN_=6334
-
+_NN_=6334d
+PROFILE=https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/profile
+CLOUDMRCMR=https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/
 
 
 # Create a bucket
 BUCKET_NAME=mro-mainbucket-$_NN_
-FRONTSTACKNAME=MROFrontstack
-
+REGION=us-east-1
+COMMONSTACKNAME=MROCommon-$_NN_
+BACKSTACKNAME=MROBackstack-$_NN_
+FRONTSTACKNAME=MROFrontstack-$_NN_
+USAGEPLANSTACKNAME=USAGEPLAN-$_NN_
 
 
 #check if $BUCKET_NAME exists
@@ -24,23 +27,48 @@ fi
 
 
 
-#frontend
 
-APITOKEN='mroptimum'
+# get the value of the api-token from the cloudformation stack
+#APITOKEN=$(aws cloudformation describe-stacks --stack-name $CLOUDMRSTACK --query "Stacks[0].Outputs[?OutputKey=='ApiToken'].OutputValue" --output text)    
+
+TOKEN_KEY=$(aws cloudformation describe-stacks --stack-name $USAGEPLANSTACKNAME --query "Stacks[0].Outputs[?OutputKey=='ApiKey'].OutputValue" --output text)
+
+APITOKEN=$(aws apigateway get-api-key --api-key $TOKEN_KEY --include-value | jq -r '.value')
+
 #cloud formaiton make an api-token
 
-PROFILE_SERVER=
-CLOUDMR_SERVER=
-MRO_SERVER=tbd
-TOKEN_URL=null
 
-GITTOKENS=token
 
-PARAMS="GithubToken=$GITTOKENS ApiToken=$APITOKEN CloudmrServer=$CLOUDMR_SERVER MroServer=$MRO_SERVER ProfileServer=$PROFILE_SERVER"
+#frontend
+PROFILE_SERVER=$(aws cloudformation describe-stacks --stack-name $CLOUDMRSTACK --query "Stacks[0].Outputs[?OutputKey=='ProfileGetAPI'].OutputValue" --output text)
+if [ $? -eq 0 ]; then
+    echo "Profile server exists"
+else
+    PROFILE_SERVER=$PROFILE
+fi
+
+
+
+CLOUDMR_SERVER=$(aws cloudformation describe-stacks --stack-name $CLOUDMRSTACK --query "Stacks[0].Outputs[?OutputKey=='CmrApi'].OutputValue" --output text)
+if [ $? -eq 0 ]; then
+    echo "cluodmr server exists"
+else
+    CLOUDMR_SERVER=$CLOUDMRCMR
+fi
+
+
+MRO_SERVER=$(aws cloudformation describe-stacks --stack-name $BACKSTACKNAME --query "Stacks[0].Outputs[?OutputKey=='MROApi'].OutputValue" --output text)
+
+
+GITTOKENS=$CMRGITTOKEN
+
+PARAMS="GithubToken=$GITTOKENS ApiToken=$APITOKEN CloudmrServer=$CLOUDMR_SERVER MroServer=$MRO_SERVER ProfileServer=$PROFILE_SERVER ApiUrl=aa"
 
 
 sam build -t Frontend/template.yaml --use-container --build-dir build/front
 sam deploy --template-file build/front/template.yaml --stack-name $FRONTSTACKNAME --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --resolve-image-repos --s3-bucket $BUCKET_NAME --parameter-overrides $PARAMS
+# sam deploy --template-file build/front/template.yaml --stack-name $FRONTSTACKNAME --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --resolve-image-repos --s3-bucket $BUCKET_NAME --parameter-overrides $(echo $PARAMS)
 
 
+FRONTEND_URL=$(aws cloudformation describe-stacks --stack-name $FRONTSTACKNAME --query "Stacks[0].Outputs[?OutputKey=='AmplifyAppDomain'].OutputValue" --output text)
 
