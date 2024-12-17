c05cee77cae5a9bbdd2b3addf541a0819572b151
Eros <eros.montin@gmail.com>
2024-12-17 14:21:56 -0500

    v1

--- mroptimum/t.py
+++
@@ -1,8 +0,0 @@
-import pyable_eros_montin.imaginable as ima
-import numpy as np  
-
-L=np.tile(np.array([1,2,3,4,5,6,7,8,9,10]),(10,1))
-# use itk to save the image and metadata
-import itk
-img=itk.GetImageFromArray(L)
-itk.imwrite(img,'test.tif')
